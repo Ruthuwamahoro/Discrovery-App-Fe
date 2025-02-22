142ecf53358c179786022727465a1c67087b6740
Ruthuwamahoro <r.uwamahoro@alustudent.com>
2025-02-22 14:19:02 +0200

    removing irrevant comments

--- src/components/BookCardSkeleton.tsx
+++ src/components/BookCardSkeleton.tsx
@@ -13,28 +13,23 @@ const BookCardSkeleton = () => {
       </div>
 
       <div className="p-4">
-        {/* Title skeleton */}
         <div className="h-6 w-3/4 bg-gray-200 rounded animate-pulse" />
         
-        {/* Author skeleton */}
         <div className="flex items-center gap-2 mt-2">
           <div className="h-4 w-4 bg-gray-200 rounded animate-pulse" />
           <div className="h-4 w-1/2 bg-gray-200 rounded animate-pulse" />
         </div>
 
-        {/* Genre and price skeleton */}
         <div className="flex justify-between items-center mt-3">
           <div className="h-6 w-16 bg-gray-200 rounded-full animate-pulse" />
           <div className="h-6 w-16 bg-gray-200 rounded animate-pulse" />
         </div>
 
-        {/* Description skeleton */}
         <div className="mt-3 space-y-2">
           <div className="h-4 w-full bg-gray-200 rounded animate-pulse" />
           <div className="h-4 w-2/3 bg-gray-200 rounded animate-pulse" />
         </div>
 
-        {/* Footer skeleton */}
         <div className="mt-4 flex justify-between items-center">
           <div className="h-3 w-24 bg-gray-200 rounded animate-pulse" />
           <div className="h-3 w-20 bg-gray-200 rounded animate-pulse" />
